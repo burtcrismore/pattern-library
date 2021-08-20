660c92290ff8a647ef1b418b99868a2f9eb98321
Burt Crismore <burt@burtcrismore.com>
2021-08-20 00:05:48 -0700

    updates to accordion and grid

--- src/js/modules/accordion.js
+++ src/js/modules/accordion.js
@@ -8,20 +8,64 @@ export function accordion() {
 		(event) => {
 			const items = event.currentTarget.querySelectorAll('.accordion__item');
 			const accordionAllOpen = event.currentTarget.classList.contains('accordion--all-open');
-			const accordionBody = event.target.closest('.accordion__header').nextElementSibling.classList;
+			const accordionButton = event.target;
+			const accordionItem = event.target.closest('.accordion__item');
+			const accordionBody = accordionItem.querySelector('.accordion__body');
 
-			if (accordionAllOpen) {
-				accordionBody.toggle('block');
-				return;
+			const accordionContainsCollapse = accordionBody.classList.contains('collapse');
+			const accordionContainsShow = accordionBody.classList.contains('show');
+			const accordionContent = accordionItem.querySelector('.accordion__content');
+			// const accordionCollapsed = accordionBody.getAttribute('data-accordion') === 'collapsed';
+			// const accordionCollapseState = accordionBody.getAttribute('data-accordion');
+			// const accordionHasAttribute = accordionBody.hasAttribute('data-accordion');
+			// const accordionPanelState = event.currentTarget.getAttribute('data-accordion');
+
+			function expand() {
+				accordionBody.classList.remove('collapse');
+				accordionBody.classList.add('transition');
+				const sectionHeight = accordionBody.scrollHeight;
+				accordionBody.style.setProperty('--height', sectionHeight + 'px');
+				console.log(sectionHeight);
+				// accordionBody.classList.remove('collapse');
+				// accordionBody.classList.add('expanding');
+				// const sectionHeight = accordionBody.scrollHeight;
+				// accordionBody.style.height = sectionHeight + 'px';
+
+				const adjustHeight = () => {
+					accordionBody.classList.remove('transition');
+					// accordionBody.classList.add('collapse');
+					accordionBody.classList.add('show');
+					// accordionBody.removeAttribute('style');
+					accordionBody.removeEventListener('transitionend', adjustHeight);
+					console.log('remove expand transition');
+				};
+				accordionBody.addEventListener('transitionend', adjustHeight);
 			}
-			if (!accordionAllOpen) {
-				for (const item of items) {
-					if (!accordionBody.contains('block')) {
-						item.children[1].classList.remove('block');
-					}
-				}
-				accordionBody.toggle('block');
-				return;
+
+			function collapse() {
+				accordionBody.classList.remove('show');
+				// accordionBody.classList.remove('collapse');
+				const sectionHeight = accordionBody.scrollHeight;
+				accordionBody.style.setProperty('--height', sectionHeight + 'px');
+				accordionBody.classList.add('transition');
+
+				// accordionBody.style.height = sectionHeight + 'px';
+
+				// accordionBody.classList.add('collapsing');
+
+				const adjustHeight = () => {
+					accordionBody.classList.remove('transition');
+					accordionBody.classList.add('collapse');
+					accordionBody.removeAttribute('style');
+					accordionBody.removeEventListener('transitionend', adjustHeight);
+				};
+				accordionBody.addEventListener('transitionend', adjustHeight);
+			}
+
+			if (accordionContainsCollapse) {
+				expand();
+			} else {
+				collapse();
 			}
 		},
 		accordions
